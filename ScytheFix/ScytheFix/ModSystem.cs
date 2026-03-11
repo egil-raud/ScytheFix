530c05c70662e4e054d22c869c97d48b4119b515
Egil Raud <@EGIL-RAUD>
2026-03-11 07:50:00 +0700

    Добавьте файлы проекта.

---
+++ ScytheFix/ScytheFix/ModSystem.cs
@@ -0,0 +1,14 @@
+﻿// ProgressOverlaySystem.cs
+using Vintagestory.API.Common;
+
+namespace ProgressOverlay
+{
+    public class ProgressOverlaySystem : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.Logger.Notification("[ScytheFix] Мод загружен!");
+        }
+    }
+}
